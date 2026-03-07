a95413210dd1fd5ad883970067265e15619551b7
ajmalbinkhalid <ajmalbinkhalid2112@gmail.com>
2026-03-07 22:13:10 +0530

    url

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
+import os
 from fastapi import FastAPI
-from src.database import Base , engine
+from src.database import Base, engine
 from src.routes import auth_routes
 from src.routes import user_routes
 from src.routes import task_routes
@@ -7,12 +8,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+frontend_url = os.getenv("FRONTEND_URL")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+    allow_origins=[frontend_url],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -25,6 +25,7 @@ app.include_router(user_routes.router)
 app.include_router(auth_routes.router)
 
 app.include_router(task_routes.router)
+
 
 @app.get("/")
 def root():
